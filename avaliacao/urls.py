634451d7965a4c05218216a3e64c07d923fa3cff
Sóstenes Melo <sostenesilvaa@gmail.com>
2024-06-08 20:50:47 -0300

    Correção de erros com importações circulares

--- avaliacao/urls.py
+++ avaliacao/urls.py
@@ -16,6 +16,4 @@ urlpatterns = [
 
     path('controle/', views.controle, name='controle'),
     path('secadm/', views.secadm, name='secadm'),
-    
-    path('enviar_email/', views.enviar_email, name='enviar_email')
 ]
